2fcec76f0f5e3563ed1f38ae222e1b5a83353f24
Дарья Нагорная <zbnsxvj@gmail.com>
2025-09-30 23:36:23 +0300

    Изменение HttpServerApp и FileExchangeApplication: базовая структура проекта и конфигурация

--- task-2-file-service/src/main/java/com/fileservice/FileExchangeApplication.java
+++ task-2-file-service/src/main/java/com/fileservice/FileExchangeApplication.java
@@ -2,4 +2,8 @@ package com.fileservice;
 
 // Точка входа в приложение
 public class FileExchangeApplication {
+    public static void main(String[] args) throws Exception {
+        java.nio.file.Files.createDirectories(java.nio.file.Paths.get("src/main/resources/files"));
+        HttpServerApp.start(8080);
+    }
 }
--- task-2-file-service/src/main/java/com/fileservice/config/HttpServerApp.java
+++ task-2-file-service/src/main/java/com/fileservice/config/HttpServerApp.java
@@ -1,5 +1,20 @@
 package com.fileservice.config;
 
+import com.fileservice.controller.*;
+import com.fileservice.service.FileStorage;
+import com.fileservice.util.StaticFileHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
 // Класс конфигурации HTTP сервера
 public class HttpServerApp {
+    public static void start(int port) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        // TODO: Добавить обработчики маршрутов
+
+        server.setExecutor(null);
+        System.out.println("Server started at http://localhost:" + port);
+        server.start();
+    }
 }

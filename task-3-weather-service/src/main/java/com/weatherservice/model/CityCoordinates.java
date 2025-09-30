aece172ce66685b6951b679ab64a83807365bc37
Дарья Нагорная <zbnsxvj@gmail.com>
2025-10-01 02:25:22 +0300

    Изменение моделей: Базовая структура проекта и модели данных

--- task-3-weather-service/src/main/java/com/weatherservice/model/CityCoordinates.java
+++ task-3-weather-service/src/main/java/com/weatherservice/model/CityCoordinates.java
@@ -1,5 +1,26 @@
 package com.weatherservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 // Модель координат города
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CityCoordinates {
+    private String name; // название города
+    private double latitude; // широта
+    private double longitude; // долгота
+
+    public CityCoordinates() {}
+
+    public CityCoordinates(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public double getLatitude() { return latitude; }
+    public void setLatitude(double latitude) { this.latitude = latitude; }
+    public double getLongitude() { return longitude; }
+    public void setLongitude(double longitude) { this.longitude = longitude; }
 }
--- task-3-weather-service/src/main/java/com/weatherservice/model/WeatherForecast.java
+++ task-3-weather-service/src/main/java/com/weatherservice/model/WeatherForecast.java
@@ -1,5 +1,41 @@
 package com.weatherservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 // Прогноз погоды
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherForecast {
+    private HourlyData hourly; // почасовые данные о погоде
+
+    public WeatherForecast() {}
+
+    public WeatherForecast(HourlyData hourly) {
+        this.hourly = hourly;
+    }
+
+    public HourlyData getHourly() { return hourly; }
+    public void setHourly(HourlyData hourly) { this.hourly = hourly; }
+
+    // Класс для хранения почасовых данных
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class HourlyData {
+        private List<String> time; // временные метки
+
+        @JsonProperty("temperature_2m")
+        private List<Double> temperature2m; // темп на высоте 2м
+
+        public HourlyData() {}
+        public HourlyData(List<String> time, List<Double> temperature2m) {
+            this.time = time;
+            this.temperature2m = temperature2m;
+        }
+
+        public List<String> getTime() { return time; }
+        public void setTime(List<String> time) { this.time = time; }
+        public List<Double> getTemperature2m() { return temperature2m; }
+        public void setTemperature2m(List<Double> temperature2m) { this.temperature2m = temperature2m; }
+    }
 }

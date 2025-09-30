48b6cbb3bed5e2217c1ebfbcde74288a2a67ff82
Дарья Нагорная <zbnsxvj@gmail.com>
2025-09-30 23:37:45 +0300

    Изменение FileMetadata: модель данных для метаданных файлов

--- task-2-file-service/src/main/java/com/fileservice/model/FileMetadata.java
+++ task-2-file-service/src/main/java/com/fileservice/model/FileMetadata.java
@@ -1,5 +1,19 @@
 package com.fileservice.model;
 
+import java.util.Date;
+
 // Модель метаданных файла
 public class FileMetadata {
+    public String id;
+    public String fileName;
+    public long fileSize;
+    public Date uploadDate;
+    public int downloadCount = 0;
+
+    public FileMetadata(String id, String fileName, long fileSize) {
+        this.id = id;
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.uploadDate = new Date();
+    }
 }

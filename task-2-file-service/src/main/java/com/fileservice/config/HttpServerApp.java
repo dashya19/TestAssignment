9fa252f5a1defb1dafd07ff012bf877bf069c0ac
Дарья Нагорная <zbnsxvj@gmail.com>
2025-09-30 23:50:21 +0300

    Обновление HttpServerApp: добавление конфигурации сервера с маршрутами

--- task-2-file-service/src/main/java/com/fileservice/config/HttpServerApp.java
+++ task-2-file-service/src/main/java/com/fileservice/config/HttpServerApp.java
@@ -11,10 +11,21 @@ public class HttpServerApp {
     public static void start(int port) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
-        // TODO: Добавить обработчики маршрутов
+        // API
+        server.createContext("/api/auth/login", new AuthHandler());
+        server.createContext("/api/upload", new UploadHandler());
+        server.createContext("/api/download", new DownloadHandler());
+        server.createContext("/api/stats", new StatsHandler());
+        server.createContext("/api/user/files", new UserFilesHandler());
+
+        // Frontend
+        server.createContext("/", new StaticFileHandler());
 
         server.setExecutor(null);
         System.out.println("Server started at http://localhost:" + port);
         server.start();
+
+        // Очистка старых файлов
+        FileStorage.startCleanupTask();
     }
 }

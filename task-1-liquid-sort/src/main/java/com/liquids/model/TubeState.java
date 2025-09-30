577f0861f993b6d751368308edabe0cb361cceb7
Дарья Нагорная <zbnsxvj@gmail.com>
2025-09-30 15:16:06 +0300

    Изменение класса TubeState: реализация состояния пробирки с базовыми функциями

--- task-1-liquid-sort/src/main/java/com/liquids/model/TubeState.java
+++ task-1-liquid-sort/src/main/java/com/liquids/model/TubeState.java
@@ -18,4 +18,66 @@ public class TubeState {
         }
         this.fillHeights = Arrays.copyOf(fillHeights, fillHeights.length);
     }
+
+    public static TubeState fromArray(int[][] input) {
+        int n = input.length;
+        int v = input[0].length;
+        int[][] tubeContents = new int[n][v];
+        int[] fillHeights = new int[n];
+
+        // Обработка каждой пробирки
+        for (int i = 0; i < n; i++) {
+            int h = 0; // высота заполнения
+            boolean seenZero = false; // пустое место
+            //Обработка каждого уровня в пробирке
+            for (int d = 0; d < v; d++) {
+                int color = input[i][d]; // цвет жидкости на текущей позиции
+                if (color == 0) {
+                    seenZero = true;
+                } else {
+                    // если после нуля ненулевое значение - ошибка
+                    if (seenZero) {
+                        throw new IllegalArgumentException(
+                                "Неверный ввод: ненулевое значение после нуля в строке " + i + "."
+                        );
+                    }
+                    h++;
+                }
+            }
+            fillHeights[i] = h;
+            // заполнение содержимого пробирки - переворот порядка
+            for (int d = 0; d < v; d++) {
+                tubeContents[i][d] = input[i][v - 1 - d];
+            }
+        }
+        return new TubeState(tubeContents, fillHeights);
+    }
+
+    public int getTotalTubes() { return totalTubes; }
+    public int getTubeCapacity() { return tubeCapacity; }
+
+    // Получение верхней капли в пробирке
+    public int getTopColor(int tubeIndex) {
+        if (fillHeights[tubeIndex] == 0) return 0;
+        return tubeContents[tubeIndex][fillHeights[tubeIndex]-1];
+    }
+
+    public int getFillHeight(int tubeIndex) { return fillHeights[tubeIndex]; }
+    public int getFreeSpace(int tubeIndex) { return tubeCapacity - fillHeights[tubeIndex]; }
+
+    // Проверка, является ли состояние решенным
+    public boolean isSolved() {
+        // Проверка каждой пробирки
+        for (int i = 0; i < totalTubes; i++) {
+            int h = fillHeights[i];
+            if (h == 0) continue; // пропуск пустой пробирки
+            if (h != tubeCapacity) return false; // если пробирка не заполнена полностью - не решено
+            int color = tubeContents[i][0]; // цвет первой капли
+            // Проверка, если в пробирке все капли одного цвета
+            for (int d = 0; d < tubeCapacity; d++) {
+                if (tubeContents[i][d] != color) return false;
+            }
+        }
+        return true;
+    }
 }

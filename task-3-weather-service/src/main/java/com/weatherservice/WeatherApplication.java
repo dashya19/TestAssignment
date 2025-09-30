746ba8e650025745deb753bc088b8a777660c529
Дарья Нагорная <zbnsxvj@gmail.com>
2025-10-01 02:34:43 +0300

    Изменение WeatherApplication: добавление главного класса приложения

--- task-3-weather-service/src/main/java/com/weatherservice/WeatherApplication.java
+++ task-3-weather-service/src/main/java/com/weatherservice/WeatherApplication.java
@@ -1,4 +1,23 @@
 package com.weatherservice;
 
+import com.weatherservice.server.WeatherHttpServer;
+import com.weatherservice.service.LocationService;
+import com.weatherservice.service.WeatherCacheService;
+import com.weatherservice.service.WeatherService;
+
 public class WeatherApplication {
+    public static void main(String[] args) {
+        try {
+            int port = 8080;
+            WeatherCacheService cacheService = new WeatherCacheService();
+            LocationService geoCodingService = new LocationService(); // сервис геолокации
+            WeatherService weatherService = new WeatherService(geoCodingService, cacheService);  // сервис погоды
+
+            WeatherHttpServer server = new WeatherHttpServer(port, weatherService);
+            System.out.println("Сервис погоды запущен на порту " + port);
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
